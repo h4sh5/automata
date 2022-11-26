524b168735b52bfc8b50028dca43afff89d10739
Haoxi Tan <haoxi.tan@gmail.com>
2022-11-26 15:55:12 +1000

    some more rules, nice harness

--- boi.py
+++ boi.py
@@ -5,7 +5,7 @@ import sys
 # graph / output array
 OUT = []
 
-TYPES = ["_", '*', ' ']
+TYPES = ["_", '*', ' ', '#']
 # generate something like this:
 #{'_': 0, '*': 1, ' ': 2}
 
@@ -14,6 +14,9 @@ TYPEDICT_R = dict([(TYPEDICT.get(k),k) for k in TYPEDICT])
 
 
 def rule1(arr):
+	'''
+	next type of each is added by index and wrapped around
+	'''
 	for i in range(len(arr)):
 		c = arr[i]
 		typeindex = TYPEDICT_R[c]
@@ -21,17 +24,73 @@ def rule1(arr):
 		arr[i] = TYPES[nexttypeindex]
 	return arr
 
+def rule2(arr):
+	for i in range(len(arr)):
+		c = arr[i]
+		typeindex = TYPEDICT_R[c]
+		# nexttypeindex = (typeindex) % len(TYPES)
+		if typeindex < 2:
+			nexttypeindex = (typeindex + i) % len(TYPES)
+			arr[i] = TYPES[nexttypeindex]
+		else:
+			nexttypeindex = (typeindex - i) % len(TYPES)
+			arr[i] = TYPES[nexttypeindex]
+	return arr
 
+def rule3(arr):
+	for i in range(len(arr)):
+		c = arr[i]
+		c_left = arr[(i+1) % len(TYPES)]
+		c_right = arr[(i-1) % len(TYPES)]
+		typeindex = TYPEDICT_R[c]
+		left_typeindex = TYPEDICT_R[c_left]
+		right_typeindex = TYPEDICT_R[c_right]
+
+		if typeindex > left_typeindex:
+			arr[i] = TYPES[(typeindex * 2) % len(TYPES)]
+		else:
+			arr[i] = TYPES[(typeindex - 1) % len(TYPES)]
+	return arr
+
+def rule4(arr):
+	for i in range(len(arr)):
+		c = arr[i]
+		c_left = arr[(i+1) % len(TYPES)]
+		c_right = arr[(i-1) % len(TYPES)]
+		typeindex = TYPEDICT_R[c]
+		left_typeindex = TYPEDICT_R[c_left]
+		right_typeindex = TYPEDICT_R[c_right]
+
+		if typeindex > left_typeindex:
+			arr[i] = TYPES[(typeindex * 2) % len(TYPES)]
+		if typeindex < right_typeindex:
+			arr[i] = TYPES[(typeindex // 2) % len(TYPES)]
+		else:
+			arr[i] = TYPES[(typeindex - 1) % len(TYPES)]
+	return arr
 
 def prettyprint(arr):
 	print(''.join(arr))
 
-init = list("_" * 50)
-count = 5
-prettyprint(init)
-for i in range(count):
-	init = rule1(init)
+def run(rulefunc, count):
+	init = list("_" * 50)
 	prettyprint(init)
+	for i in range(count):
+		init = rule1(init)
+		prettyprint(init)
 
 
+count = 10
 
+
+print("----------- rule1 ----------------")
+run(rule1, count)
+
+print("----------- rule2 ----------------")
+run(rule2, count)
+
+print("----------- rule3 ----------------")
+run(rule3, count)
+
+print("----------- rule4 ----------------")
+run(rule4, count)
